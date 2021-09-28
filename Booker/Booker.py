42f212809fe7f84e1b816840529b6a06a98482a6
lukeo102 <lukeormiston@gmail.com>
2021-09-28 21:13:29 +0100

    Comment update

--- Booker/Booker.py
+++ Booker/Booker.py
@@ -125,17 +125,3 @@ else:
 rooms = user.get_rooms()
 print(rooms)
 """
-
-"""
-
-TODO
-
-    get encryption for passwords
-    randomly select an account for a day (that hasn't already been selected for that day)(3 accounts are required for a full booking from 9am till 6pm)
-    confirm the program *actually* works properly
-
-HOW WILL THIS WORK
-
-    Server runs a script at like 00:01, said script runs this program then 
-
-"""
